09eeb7a393e9f850c90089ad66ef52af353119ef
Ellen Lehman <ellenklehman@users.noreply.github.com>
2014-08-03 12:12:05 -0700

    remove comments, add spacing and rename variable as per Epicodus standards

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-//The scrabbleConverter function will take a user-inputted word and calculate the correct scrabble score for that word.
 var scrabbleConverter = function(userWord) {
   var letterValues = {"a": 1, "e": 1, "i": 1, "o": 1, "u": 1, "l": 1, "n": 1, "r": 1, "s": 1, "t": 1, 
                       "d": 2, "g": 2, 
@@ -8,24 +7,14 @@ var scrabbleConverter = function(userWord) {
                       "j": 8, "x": 8, 
                       "q": 10,"z": 10};
 
-  var scores = 0; /*scores is a placeholder to collect and add the values of the letters*/
+  var score = 0; 
 
-//The for loop runs through each letter of the userword, 
-//selects it in letterScores and returns the value attached to that letter,
-//and adds it to the scores variable.
-
-  for(var i = 0; i<userWord.length; i++) {
-    scores += letterValues[userWord[i].toLowerCase()];
+  for (var i = 0; i < userWord.length; i++ ) {
+    score += letterValues[userWord[i].toLowerCase()];
   }
+  return score;
+  };
 
-
-  console.log(scores);
-  return scores;
-
-};
-
-//Collects user input from the scrabble-word id and puts it as an argument into the scrabbleConverter function.
-//Returns the resulting number on the webpage
 $(document).ready(function() {
   $("form#scrabble-input").submit(function (event) {
     var userInputWord = $("input#scrabble-word").val();
@@ -34,6 +23,5 @@ $(document).ready(function() {
     $("#result-text").text(endScore);
 
     event.preventDefault();
-
   });
 });
