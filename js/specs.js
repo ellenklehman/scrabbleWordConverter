94c6e4793b40c9949970616346ecfa2e76397c6a
Ellen Lehman <ellenklehman@users.noreply.github.com>
2014-08-03 12:13:22 -0700

    rephrase 'it' statements and add appropriate spacing

--- js/specs.js
+++ js/specs.js
@@ -1,8 +1,9 @@
 describe("scrabbleConverter", function() {
-  it ("should attach a value to a given letter", function () {
+  it ("attaches a value to a given letter", function () {
     scrabbleConverter("j").should.equal(8);
   });
-  it("should return a sum of the letters in a word", function () {
+
+  it("returns a sum of the letters in a word", function () {
     scrabbleConverter("doo").should.equal(4);
   });
 });
